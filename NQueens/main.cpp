b869efc3c1d3ce7c1f32c2791e04691950ff75f5
Mohamed Radhi ZOUAOUI <radhi@Radhi-MBP.home>
2015-04-22 21:43:33 +0100

    Initial Commit

---
+++ NQueens/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NQueens
+//
+//  Created by Mohamed Radhi ZOUAOUI on 22/04/2015.
+//  Copyright (c) 2015 Mohamed Radhi ZOUAOUI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
